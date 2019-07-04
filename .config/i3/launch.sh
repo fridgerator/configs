d16ccdbcdd5c9f239ee857119d533ad99aedaf4d
Nick Franken <shnick@gmail.com>
2019-07-04 12:32:53 -0500

    added backup script

--- .config/i3/launch.sh
+++ .config/i3/launch.sh
@@ -2,6 +2,6 @@
 
 xrandr && xrandr --output eDP1 --mode 2560x1440
 nitrogen --restore
-compton --config ~/.config/compton/compton.conf --backend glx --blur-background --blur-method kawase --blur-strength 8 &
+compton --config ~/.config/compton/compton.conf #--backend glx --blur-background --blur-method kawase --blur-strength 8 &
 # /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg) &
 
---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+cp -r ~/.config/cava .config/
+cp -r ~/.config/polybar .config/
+cp -r ~/.config/i3 .config/
+cp -r ~/.config/compton .config/
+
