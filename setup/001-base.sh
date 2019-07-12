e10010affe65498329fe1d11f33f67ad6e8fe711
Nick Franken <shnick@gmail.com>
2019-07-12 15:17:50 -0500

    setup scripts

---
+++ setup/001-base.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apps=(
+i3-gaps
+compton-tryone-git
+playerctl
+polybar
+cava
+nitrogen
+python-pywal
+google-chrome
+ttf-font-awesome
+neovim
+code
+)
+
+./setup/install-app.sh ${apps[*]}
